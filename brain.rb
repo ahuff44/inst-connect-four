34facd2f055ad470290a24142aac20f92a04431c
Andrew Huff <andrewhuff4@gmail.com>
2016-07-26 12:53:57 -0600

    WIP can simulate_move

--- brain.rb
+++ brain.rb
@@ -1,17 +1,41 @@
 $arr = [[1,2,3], [4,5,6]]
 $b1 = [
-    ["0", "0", "0", "0", ],
-    ["1", "0", "0", "2", ],
-    ["2", "0", "1", "1", ],
-    ["1", "1", "2", "2", ],
+    [0, 0, 0, 0, 0, 2],
+    [1, 0, 0, 2, 0, 1],
+    [2, 0, 1, 1, 0, 1],
+    [1, 1, 2, 2, 0, 1],
 ]
 
 def get_move(board)
   height = board.length
   width = board[0].length
   choices = valid_moves(board)
-  puts choices
   choices.sample
+end
+
+def clone_board(board)
+  board.each_with_object([]) do |row, new_board|
+    new_board << row.clone
+  end
+end
+
+def simulate_move(board, cc, player)
+  # assert cc in valid_moves(board)
+
+  new_board = clone_board(board)
+  col = (new_board.transpose)[cc]
+  index = col.find_index { |e| e != 0 }
+  if index == 0
+    raise "IAE"
+    nil
+  elsif index.nil? # column empty
+    index = col.length - 1
+  else
+    index = index - 1
+  end
+
+  new_board[index][cc] = player
+  new_board
 end
 
 def valid_moves(board)
