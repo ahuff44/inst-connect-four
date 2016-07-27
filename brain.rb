4a2a4f91101f546e56e2556f4e12ca6b90f7d6fb
Andrew Huff <andrewhuff4@gmail.com>
2016-07-27 14:13:39 -0600

    refactored depth out; pushes as "bot:betterdoubles"

--- brain.rb
+++ brain.rb
@@ -1,23 +1,23 @@
 # Public
-def get_move(board, me, depth=1)
-  good, nice, meh, semibad, bad = get_move_helper(board, me, depth)
+def get_move(board, me)
+  good, nice, meh, semibad, bad = get_move_helper(board, me)
   choices = [good, nice, meh, semibad, bad].reject{ |moves| moves.empty? }.first
   random_choice(board, me, choices)
 end
 
-def get_move_helper(board, me, depth=1)
+def get_move_helper(board, me)
   him = (me == 1) ? 2 : 1
 
   meh = valid_moves(board)
 
   ### 1.1 Win
-  good = meh.select { |cc| winner(simulate_move(board, cc, me)) == me }
+  good = get_winning_moves(board, me, meh)
   if !good.empty?
     return [good, [], [], [], []]
   end
 
   ### 1.2 Block
-  good = meh.select { |cc| winner(simulate_move(board, cc, him)) == him }
+  good = get_winning_moves(board, him, meh)
   if !good.empty?
     return [good, [], [], [], []]
   end
@@ -34,37 +34,34 @@ def get_move_helper(board, me, depth=1)
   end
 
   ### 2.1 Create doubles
-  if depth > 0
-    nice, meh = meh.clone.partition do |cc|
-      # begin
-      board_1 = simulate_move(board, cc, me)
-      _good, _nice, _meh, _semibad, _bad = get_move_helper(board_1, me, 0)
-      _good.length >= 2 # if I was allowed to go again, are there two ways to win?
-
-      # rescue NoMethodError => e
-      # end
-    end
-    if !nice.empty?
-      return [[], nice, [], [], []]
-    end
+  nice, meh = meh.clone.partition do |cc|
+    board_1 = simulate_move(board, cc, me)
+    wins = get_winning_moves(board_1, me, valid_moves(board_1))
+    wins.length >= 2 # if I was allowed to go again, are there two ways to win?
+  end
+  if !nice.empty?
+    return [[], nice, [], [], []]
   end
 
   ### 2.2 Avoid allowing blocking
-  semibad = []
-  if depth > 0
-    semibad, meh = meh.clone.partition do |cc|
-      board_2 = simulate_move(board, cc, him)
-      board_21 = simulate_move(board_2, cc, me)
-      if board_21.nil?
-        false # simulation failed
-      else
-        winner(board_21) == me
-        # leave in the tension - he can't go there (b/c when I play on top of him there I get 4-in-a-row)
-      end
+  semibad, meh = meh.clone.partition do |cc|
+    board_2 = simulate_move(board, cc, him)
+    board_21 = simulate_move(board_2, cc, me)
+    if board_21.nil?
+      false # simulation failed
+    else
+      winner(board_21) == me
+      # leave in the tension - he can't go there (b/c when I play on top of him there I get 4-in-a-row)
     end
   end
 
   return [[], [], meh, semibad, bad]
+end
+
+def get_winning_moves(board, me, candidates)
+  # returns a list of moves that win.
+  # this is a helper method for get_move
+  candidates.select { |cc| winner(simulate_move(board, cc, me)) == me }
 end
 
 def random_choice(board, me, choices)
